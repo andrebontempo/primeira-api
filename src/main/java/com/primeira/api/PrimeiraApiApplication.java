adcc1f096ae6a5151f3d8bb5d973d915d909b064
andrebontempo <andre.lnx@gmail.com>
2022-03-04 08:26:42 -0300

    Primeiro Commit

---
+++ src/main/java/com/primeira/api/PrimeiraApiApplication.java
@@ -0,0 +1,15 @@
+package com.primeira.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraApiApplication.class, args);
+		
+		
+	}
+
+}
